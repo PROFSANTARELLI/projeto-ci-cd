8b476561a649beff0461c2bd6d2931af8d8a5b08
FLAVIO SANTARELLI <145938765+PROFSANTARELLI@users.noreply.github.com>
2025-04-16 18:59:51 -0300

    Create player.py

---
+++ player.py
@@ -0,0 +1,9 @@
+class Player: 
+    def __init__(self, name, hp): 
+        self.name = name 
+        self.hp = hp 
+ 
+    def take_damage(self, amount): 
+        self.hp -= amount 
+        if self.hp < 0: 
+            self.hp = 0
